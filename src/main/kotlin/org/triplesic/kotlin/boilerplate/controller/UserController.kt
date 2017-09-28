547a93703db74a82c1f9373e10ac03e0c576a980
Napasin Yamwat <triplesic@gmail.com>
2017-09-29 03:38:53 +0700

    update rest controller example

--- src/main/kotlin/org/triplesic/kotlin/boilerplate/controller/UserController.kt
+++ src/main/kotlin/org/triplesic/kotlin/boilerplate/controller/UserController.kt
@@ -1,13 +1,15 @@
 package org.triplesic.kotlin.boilerplate.controller
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController class UserController @Autowired constructor(){
     @GetMapping("/users")
-    fun getAllUsers(): String{
-        return "ABC"
+    fun getAllUsers(): ResponseEntity<Map<Int,String>>{
+
+        return ResponseEntity.ok(mapOf(1 to "A", 2 to "B"))
     }
 }
